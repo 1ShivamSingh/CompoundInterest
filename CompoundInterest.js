b7df5b502f6c88a333948e6d21dde84942c1cc0c
1ShivamSingh <shivamsikarwar2000@gmail.com>
2021-06-17 21:45:05 +0530

    Compound Interest Webpage

---
+++ CompoundInterest.js
@@ -0,0 +1,19 @@
+let PrincipleField = document.querySelector('#principal');
+let RateField = document.querySelector('#rate');
+let TimeField = document.querySelector('#time');
+let NumberField = document.querySelector('#numb');
+
+function Calculation() {
+    let p = Number(PrincipleField.value);
+    let r = Number(RateField.value);
+    let t = Number(TimeField.value);
+    let n = Number(NumberField.value);
+    let x = p * ((1 + (r / n)) ** (n * t))
+    x = x.toFixed(2);
+    document.getElementById('ans').innerHTML =
+        "The Final Amount after C.I is " + x + " Rupees";
+    PrincipleField.value = "";
+    RateField.value = "";
+    TimeField.value = "";
+    NumberField.value = "";
+}
