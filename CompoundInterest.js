9f8cb989cbf66eb8409804cd7ba087baac645066
1ShivamSingh <shivamsikarwar2000@gmail.com>
2021-06-21 20:25:18 +0530

    removed the number field

--- CompoundInterest.js
+++ CompoundInterest.js
@@ -1,21 +1,18 @@
 let PrincipleField = document.querySelector('#principal');
 let RateField = document.querySelector('#rate');
 let TimeField = document.querySelector('#time');
-let NumberField = document.querySelector('#numb');
 
 function Calculation() {
     let p = Number(PrincipleField.value);
     let r = Number(RateField.value);
     let t = Number(TimeField.value);
-    let n = Number(NumberField.value);
-    let x = p * ((1 + (r / n)) ** (n * t))
+    let x = p * ((1 + (r) ** ( t))
     x = x.toFixed(3);
     document.getElementById('ans').innerHTML =
-        "The Final Amount after C.I is " + x + " Rupees";
+        "The Final Amount after Compounded Interest is " + x + " Rupees";
     PrincipleField.value = "";
     RateField.value = "";
     TimeField.value = "";
-    NumberField.value = "";
 }
 //Checking Git Commit via VS
 //3rd Commit
